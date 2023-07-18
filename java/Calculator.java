2a3a84fdc636517fa1e561b8d588626300ed3715
hubert <hubstr@gmail.com>
2023-07-18 07:29:30 +0200

    java

---
+++ java/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+    public static void main(String... args) {
+
+        Integer a = Integer.parseInt(args[0]);
+        String operation = args[1];
+        Integer b = Integer.parseInt(args[2]);
+
+        if (operation.equals("und")) {
+            System.out.println(a + b);
+        } else if (operation.equals("weniga")) {
+            System.out.println(a - b);
+        } else if (operation.equals("moi")) {
+            System.out.println(a * b);
+        } else if (operation.equals("duach")) {
+            System.out.println(a / b);
+        }
+
+    }
+
+}
---
+++ java/InputParameters.java
@@ -0,0 +1,14 @@
+
+public class InputParameters {
+
+    public static void main(String... args) {
+
+        for (String parameter : args) {
+            System.out.println(parameter);
+        }
+
+    }
+
+}
+
+// java InputParameters
